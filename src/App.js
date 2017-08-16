3b4bf00721a26b80d8da9b57ba4e0d58a3d2db53
Adam Kipnis <adamkipnis@yahoo.com>
2017-08-15 18:17:00 -0700

    Initial App creation and Firebase/Redux setup

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+
+import reducers from './reducers';
+
+class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB_xfAQPLruihcPO3s5EvB3fKDbbWFI89A',
+      authDomain: 'employee-manager-377dc.firebaseapp.com',
+      databaseURL: 'https://employee-manager-377dc.firebaseio.com',
+      projectId: 'employee-manager-377dc',
+      storageBucket: 'employee-manager-377dc.appspot.com',
+      messagingSenderId: '722245297980'
+    });
+  }
+
+  render() {
+    return (
+        <Provider store={createStore(reducers)}>
+          <View>
+            <Text>
+              Hello!
+            </Text>
+          </View>
+        </Provider>
+    )
+  }
+}
+
+export default App;
