2faf10a8c3c1a55f7540d9fba1e169404e229e92
Adam Kipnis <adamkipnis@yahoo.com>
2017-08-20 14:18:45 -0700

    Created generic confirmation modal

---
+++ src/components/common/ConfirmationModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, View, Modal, StyleSheet } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const ConfirmationModal = (props) => {
+  return (
+      <Modal transparent animationType="slide"
+             visible={props.visible}
+             onRequestClose={() => {}}>
+        <View style={styles.containerStyle}>
+          <CardSection style={styles.cardSectionStyle}>
+            <Text style={styles.textStyle}>{props.children}</Text>
+          </CardSection>
+
+          <CardSection>
+            <Button onPress={props.onAccept}>Yes</Button>
+            <Button onPress={props.onReject}>No</Button>
+          </CardSection>
+        </View>
+      </Modal>
+  )
+};
+
+const styles = StyleSheet.create({
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+});
+
+export { ConfirmationModal };
